557f10450cbd997a8fc39a98a05c885034152b1a
ituatech <53702365+ituatech@users.noreply.github.com>
2019-08-15 14:17:03 +0200

    new java

--- src/main/java/com/it_uatech/app/servlets/AddServlet.java
+++ src/main/java/com/it_uatech/app/servlets/AddServlet.java
@@ -1,4 +1,16 @@
 package com.it_uatech.app.servlets;
 
-public class AddServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("Views/add.jsp");
+        requestDispatcher.forward(req, resp);
+    }
 }
